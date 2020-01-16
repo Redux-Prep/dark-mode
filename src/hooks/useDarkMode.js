c7980913e7b2d5a6bdbe706705705d785e01550c
Justin Trombley <jtrombley96@gmail.com>
2020-01-15 23:04:57 -0500

    useDarkMode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,20 @@
-import {useLocalStorage} from './useLocalStorage'
+import { useLocalStorage, useEffect } from './useLocalStorage';
 
-export const useDarkMode = (key) => {
-    const [value, setValue] = useLocalStorage()
-}
+export const useDarkMode = key => {
+	const [
+		value,
+		setValue,
+	] = useLocalStorage(key);
+
+	useEffect(() => {
+		if (useLocalStorage) {
+			document.body.className = 'dark-mode';
+		}
+		else {
+			document.body.className = '';
+		}
+    }, []);
+    return [
+        value, setValue
+    ]
+};
